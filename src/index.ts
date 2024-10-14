3bb44eca11e889cdff8f1d03e87c9615599a5550
Eugene-Forest <1244303915@qq.com>
2024-10-14 16:11:21 +0800

    refactor：取消 hiprint 类型的导出

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 import { App, Plugin } from "vue";
 
-// 引入组件样式
-import "@sv-print/hiprint/dist/style.css";
 import { TsDesigner } from "./components/Design";
 import { TsRender } from "./components/Render";
 import { TsMain } from "./components/Main";
-import { hiprint } from "@sv-print/hiprint";
 
 const components = [TsDesigner, TsRender, TsMain];
 
@@ -22,4 +19,3 @@ export default TsHiPrint;
 export * from "./components";
 export * from "./interface";
 
-export { hiprint };
